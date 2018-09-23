01ddbab3846df10a58cf0e824b9ae80b10ab4cfe
Vinícius Tonello Rech <viniciusrech@alunos.utfpr.edu.br>
2018-09-23 17:27:10 -0300

    a

---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,13 @@
+@extends('site.templates.app')
+@section('content')
+
+    <div class="container">
+        <div class="content">
+            <div class="col-md-12 menu-top">TESTE</div>
+            <div class="col-md-12 menu-top">TESTE</div>
+            <div class="col-md-12 menu-top">TESTE</div>
+            <div class="col-md-12 menu-top">TESTE</div>
+            <div class="col-md-12 menu-top"></div>
+        </div>
+    </div>
+@endsection
